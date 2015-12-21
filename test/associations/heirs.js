f96738fa27452d7eae583e1764bbac9085b913af
Dallas Read <dallas@excitecreative.ca>
2015-12-21 17:06:02 -0400

    Clean up heir tests.

--- test/associations/heirs.js
+++ test/associations/heirs.js
@@ -25,12 +25,13 @@ describe('Inheritable Parent', function () {
             }
         });
 
+        User.belongsTo(Org);
         Task.belongsTo(Project);
 
         S.resetTestDB(done);
     });
 
-    it('manages child perm when parent model is created or deleted', function(done) {
+    it('manages same-model bubbling for models', function(done) {
         User.create({}).then(function (user) {
             Project.create().then(function (project) {
                 user.permit('view', project).then(function () {
@@ -61,12 +62,11 @@ describe('Inheritable Parent', function () {
         });
     });
 
-    it('manages child perm when parent perm is created, updated, or deleted', function(done) {
-        // EVERY MODEL INHERITS BY DEFAULT
+    it('manages same model bubbling for perms', function(done) {
         User.create({}).then(function (user) {
             Project.create().then(function (project) {
                 Project.create({ parentId: project.id }).then(function (subProject) {
-                    user.permit('admin', project, function () { /// CHANGE THIS TO PERMIT ORG INSTEAD
+                    user.permit('admin', project, function () {
                         user.isPermittedTo('admin', subProject).then(function(permitted) { // <=
                             permitted.should.be.eql(true);
                             // console.log('SAME-MODEL BUBBLING DOWN ON PERM CREATE');
@@ -100,11 +100,76 @@ describe('Inheritable Parent', function () {
         });
     });
 
-    // console.log('DIFFERENT-MODEL BUBBLING DOWN ON MODEL CREATE');
-    // console.log('DIFFERENT-MODEL BUBBLING DOWN ON MODEL UPDATE');
-    // console.log('DIFFERENT-MODEL BUBBLING DOWN ON MODEL DELETE');
-    //
-    // console.log('DIFFERENT-MODEL BUBBLING DOWN ON PERM CREATE');
-    // console.log('DIFFERENT-MODEL BUBBLING DOWN ON PERM UPDATE');
-    // console.log('DIFFERENT-MODEL BUBBLING DOWN ON PERM DELETE');
+    it('manages different-model bubbling for models', function(done) {
+        Org.create({}).then(function(org) {
+            User.create({}).then(function (user) {
+                user.permit('view', org).then(function () {
+                    Project.create().then(function (project) {
+                        Task.create().then(function (task) {
+                            user.isPermittedTo('view', task).then(function(permitted) {
+                                permitted.should.eql(true);
+                                console.log('DIFFERENT-MODEL BUBBLING DOWN ON MODEL CREATE');
+
+                                done();
+
+                                // project.update({ inheritPerms: 0 }).then(function() {
+                                //     user.isPermittedTo('view', task).then(function(permitted) {
+                                //         permitted.should.eql(true);
+                                //         console.log('DIFFERENT-MODEL BUBBLING DOWN ON MODEL UPDATE');
+
+                                //         task.destroy().then(function() {
+                                //             user.isPermittedTo('view', task).then(function(permitted) {
+                                //                 permitted.should.eql(false);
+                                //                 console.log('DIFFERENT-MODEL BUBBLING DOWN ON MODEL DELETE');
+
+                                //                 done();
+                                //             });
+                                //         });
+                                //     });
+                                // });
+                            });
+                        });
+                    });
+                });
+            });
+        });
+    });
+
+    it('manages different-model bubbling for perms', function(done) {
+        Org.create({}).then(function (org) {
+            User.create({}).then(function (user) {
+                Project.create({}).then(function (project) {
+                    user.permit('admin', org, function () {
+                        user.isPermittedTo('admin', project).then(function(permitted) { // <=
+                            permitted.should.be.eql(true);
+                            console.log('DIFFERENT-MODEL BUBBLING DOWN ON PERM CREATE');
+
+                            user.permit('view', org, function () {
+                                user.isPermittedTo('admin', project).then(function(permitted) {
+                                    permitted.should.be.eql(false);
+                                    console.log('DIFFERENT-MODEL BUBBLING DOWN ON PERM UPDATE');
+
+                                    user.permit('admin', org, function () {
+                                        user.isPermittedTo('admin', project).then(function(permitted) {
+                                            permitted.should.be.eql(true);
+                                            console.log('DIFFERENT-MODEL BUBBLING DOWN ON PERM UPDATE (FOR THE SAKE OF THE TEST)');
+
+                                            user.unpermit(org, function () {
+                                                user.isPermittedTo('view', project).then(function(permitted) {
+                                                    permitted.should.be.eql(false);
+                                                    console.log('DIFFERENT-MODEL BUBBLING DOWN ON PERM DELETE');
+
+                                                    done();
+                                                });
+                                            });
+                                        });
+                                    });
+                                });
+                            });
+                        });
+                    });
+                });
+            });
+        });
+    });
 });
